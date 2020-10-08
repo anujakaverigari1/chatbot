254250264ee77027dcc20b8f230c1bf7e919a2c0
anuja <anujakaverigari9@gmail.com>
2020-10-08 16:36:03 +0530

    changed chatbot

--- src/app/chat-bot/chat-bot.component.ts
+++ src/app/chat-bot/chat-bot.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../service/authentication.service';
 import { QueryService } from '../service/query.service';
+declare let window: any; 
 
 export class Query{
   constructor(
@@ -14,6 +15,10 @@ export class Query{
   styleUrls: ['./chat-bot.component.css']
 })
 export class ChatBotComponent implements OnInit {
+  public message;
+  sendMessage(){
+    console.log(this.message)
+  }
   q:Query;
   query: any;
   email: any;
@@ -23,6 +28,14 @@ export class ChatBotComponent implements OnInit {
 
   ngOnInit(): void {
     this.email = this.authenticationservice.email();
+    (function(d, m){
+      var kommunicateSettings = 
+          {"appId":"3234d55ac5a78ce5678c7ea59bbbda70d","popupWidget":true,"automaticChatOpenOnNavigation":true};
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      window.kommunicate = m; m._globals = kommunicateSettings;
+  })(document, window.kommunicate || {});
   }
 
   sendQuery(query){
